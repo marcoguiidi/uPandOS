65a59d316ee9c8240a09f77e449879f116549231
marcoguiidi <marco.guidi24@studio.unibo.it>
2023-11-24 09:43:49 +0100

    initMsgs, freeMsg, allocMsg

--- phase 1/starterKitUmps3/starterKit/phase1/msg.c
+++ phase 1/starterKitUmps3/starterKit/phase1/msg.c
@@ -4,12 +4,25 @@ static msg_t msgTable[MAXMESSAGES];
 LIST_HEAD(msgFree_h);
 
 void initMsgs() {
+    for (int i = 0; i < MAXMESSAGES; i++){
+        list_add_tail(&msgTable[i].m_list, &msgFree_h);
+    }
 }
 
 void freeMsg(msg_t *m) {
+    list_add_tail(&m->m_list, &msgFree_h);
 }
 
 msg_t *allocMsg() {
+    if (list_empty(&msgFree_h) == 1){
+        return NULL;
+    }
+
+    else{
+        msg_t* removed = container_of(&msgFree_h, msg_t, m_list);
+        list_del(&removed->m_list);
+        return removed;
+    }
 }
 
 void mkEmptyMessageQ(struct list_head *head) {
