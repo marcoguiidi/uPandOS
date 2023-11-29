8a0f649f1dccbea8bb353911874f985779d42590
marcoguiidi <marco.guidi24@studio.unibo.it>
2023-11-29 16:25:36 +0100

    ultime funzioni

--- phase 1/starterKitUmps3/starterKit/phase1/msg.c
+++ phase 1/starterKitUmps3/starterKit/phase1/msg.c
@@ -31,9 +31,8 @@ msg_t *allocMsg() {
 }
 
 void mkEmptyMessageQ(struct list_head *head) {
-    struct list_head queue = LIST_HEAD_INIT(queue);
-    head = &queue;
-    return NULL;
+    head->prev = head;
+    head->next = head;
 }
 
 int emptyMessageQ(struct list_head *head) {
@@ -45,10 +44,30 @@ void insertMessage(struct list_head *head, msg_t *m) {
 }
 
 void pushMessage(struct list_head *head, msg_t *m) {
+    list_add(&m->m_list, head);
 }
 
 msg_t *popMessage(struct list_head *head, pcb_t *p_ptr) {
+    if(!list_empty(head)){
+        struct list_head* iter;
+        list_for_each(iter, head) {
+            msg_t* mess = container_of(iter, msg_t, m_list);
+            if (p_ptr == NULL){
+                list_del(iter);
+                return mess;
+            }
+            else if (p_ptr == mess->m_sender){
+            list_del(iter);
+                return mess;
+            }
+        }
+    }
+    return NULL;
 }
 
 msg_t *headMessage(struct list_head *head) {
+    if (emptyMessageQ(head)) return NULL; 
+
+    return container_of(head->next, msg_t, m_list);
+
 }
