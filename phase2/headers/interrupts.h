39dd0ae9bb778d211e239fe5aa308f2dcbbcfe61
marcoguiidi <marco.guidi24@studio.unibo.it>
2024-03-11 19:46:54 +0100

    interrupt start

--- phase2/headers/interrupts.h
+++ phase2/headers/interrupts.h
@@ -1,4 +1,10 @@
 #ifndef INTERRUPTS_H_INCLUDED
 #define INTERRUPTS_H_INCLUDED
 
+#include <umps3/umps/types.h>
+
+void interruptHandler();
+
+void nonTimerInterrupt();
+
 #endif
--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -3,3 +3,16 @@ interrupts.c This module implements the device/timer interrupt exception handler
 module will process all the device/timer interrupts, converting device/timer interrupts into ap-
 propriate messages for the blocked PCBs.
 */
+
+#include "./headers/interrupts.h"
+#include <umps3/umps/libumps.h>
+#include <umps3/umps/types.h>
+
+
+void interruptHandler(){
+    
+}
+
+void nonTimerInterrupt(){
+
+}
