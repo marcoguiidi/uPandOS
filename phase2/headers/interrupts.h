9179d642db3be49a7c3e94bb443824356d2b74ec
marcoguiidi <marco.guidi24@studio.unibo.it>
2024-03-12 18:11:45 +0100

    inizio interrupt

--- phase2/headers/interrupts.h
+++ phase2/headers/interrupts.h
@@ -2,9 +2,20 @@
 #define INTERRUPTS_H_INCLUDED
 
 #include <umps3/umps/types.h>
+#include "initial.h"
+#include <umps3/umps/const.h>
+#include <umps3/umps/cp0.h>
+#include <umps3/umps/libumps.h>
+#include <umps3/umps/types.h>
+
+
 
 void interruptHandler();
 
-void nonTimerInterrupt();
+unsigned int getIp();
+
+void nonTimerInterrupt(int);
+
+void timerInterrupt(int);
 
 #endif
--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -6,13 +6,61 @@ propriate messages for the blocked PCBs.
 
 #include "./headers/interrupts.h"
 #include <umps3/umps/libumps.h>
-#include <umps3/umps/types.h>
 
 
 void interruptHandler(){
     
+    while(1){ // ciclo continuo dell'interrupt handler
+        int line=1; // parto da 1 così ottengo subito la priorità maggiore
+        while (line < 8){
+            if (CAUSE_IP(line)){
+                break;
+            }
+            line++;
+        }
+
+        if (STATUS_IEc == 1 && STATUS_IM(line) == 1) {
+            // TODO generates interrupt exception
+        }
+
+            if (line > 2){
+                nonTimerInterrupt(line);
+        }else{
+            timerInterrupt(line);
+        }
+    }
+    
 }
 
-void nonTimerInterrupt(){
+
+
+void nonTimerInterrupt(int line){
+
+    /* 1
+    * calculate address of this device device's register
+    */
+    unsigned int devAddrBase;
+    int devNo; //TODO
+    devAddrBase = 0x10000054 + (line * 0x80) + (devNo * 0x10);
+
+    /* 2
+    * save off status code
+    */
+    unsigned int statusCode;
+    devreg_t* devReg = devAddrBase;
+    if (line == 7){
+        statusCode = devReg->term.transm_status;
+    }else{
+        statusCode = devReg->dtp.status;
+    }
+
+    /* 3
+    * acknowledge the outstanding interrupt
+    */
+    if (line == 7){
+        devReg->term.transm_command = ACK;
+    }else{
+        devReg->dtp.command = ACK;
+    }
 
 }
