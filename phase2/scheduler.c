9b0fe232851d006a4831c639c9c7cc480f0d5c5d
Marta <martabarbieri13@gmail.com>
2024-05-06 16:04:34 +0200

    scheduler

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -1,3 +1,32 @@
 /*
 scheduler.c This module implements the Scheduler and the deadlock detector.
 */
+#include "./headers/initial.h"
+#include "./headers/scheduler.h"
+#include "../phase1/headers/pcb.h"
+
+#include "/usr/include/umps3/umps/types.h"
+#include "/usr/include/umps3/umps/libumps.h"
+#include "/usr/include/umps3/umps/const.h"
+#include "/usr/include/umps3/umps/cp0.h"
+
+
+void scheduler(){
+    if(emptyProcQ(&ready_queue)==0){//se la funzione che verifica se la coda dei processi e' vuota da falso 
+    current_process=removeProcQ(&ready_queue);
+    setTIMER(TIMESLICE);
+    LDST(&(current_process->p_s.status));
+    }else{
+        if(process_count==1 && current_process->p_pid==ssi_pcb->p_pid){ 
+            HALT();
+        }
+        if(process_count>1 && soft_block_count>0){
+           setSTATUS(getSTATUS () | STATUS_IEc & STATUS_IM_MASK) & (~STATUS_TE);  
+            WAIT(); 
+        }
+        //deadlock
+        if(process_count>0 && soft_block_count==0){
+            PANIC();
+        }   
+    }
+}
