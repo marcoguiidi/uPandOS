4c0cb0915b73db3e3b340873e46a497e1c8b2fc6
Fabio Murer <murer.fabio23@gmail.com>
2024-05-10 22:06:44 +0200

    .

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -12,25 +12,23 @@ scheduler.c This module implements the Scheduler and the deadlock detector.
 
 
 void scheduler(){
-    if(emptyProcQ(&ready_queue)==FALSE){//se la funzione che verifica se la coda dei processi e' vuota da falso 
-        // rimetto il processo corrente in attesa 
+    // rimetto il processo corrente in attesa 
+    
+    
+    if(process_count==1 && current_process->p_pid==ssi_pcb->p_pid){ 
+        HALT();
+    }
+    else if(process_count>1 && soft_block_count>0){
+        setSTATUS((getSTATUS() | STATUS_IEc | STATUS_IM_MASK) & (~STATUS_TE));  
+        WAIT(); 
+    }
+    //deadlock
+    else if(process_count>0 && soft_block_count==0){
+        PANIC();
+    } else {
         insertProcQ(&ready_queue, current_process);
-
-
         current_process=removeProcQ(&ready_queue);
         setTIMER(TIMESLICE);
         LDST(&current_process->p_s);
-    }else{
-        if(process_count==1 && current_process->p_pid==ssi_pcb->p_pid){ 
-            HALT();
-        }
-        if(process_count>1 && soft_block_count>0){
-           setSTATUS((getSTATUS() | STATUS_IEc | STATUS_IM_MASK) & (~STATUS_TE));  
-            WAIT(); 
-        }
-        //deadlock
-        if(process_count>0 && soft_block_count==0){
-            PANIC();
-        }   
-    }
+    }  
 }
