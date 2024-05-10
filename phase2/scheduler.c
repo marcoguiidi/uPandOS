dacedda5709a4c07ad7ca0655d354b62fdea97f9
Fabio Murer <murer.fabio23@gmail.com>
2024-05-10 21:58:41 +0200

    sistemazioni

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -12,16 +12,20 @@ scheduler.c This module implements the Scheduler and the deadlock detector.
 
 
 void scheduler(){
-    if(emptyProcQ(&ready_queue)==0){//se la funzione che verifica se la coda dei processi e' vuota da falso 
-    current_process=removeProcQ(&ready_queue);
-    setTIMER(TIMESLICE);
-    LDST(&(current_process->p_s.status));
+    if(emptyProcQ(&ready_queue)==FALSE){//se la funzione che verifica se la coda dei processi e' vuota da falso 
+        // rimetto il processo corrente in attesa 
+        insertProcQ(&ready_queue, current_process);
+
+
+        current_process=removeProcQ(&ready_queue);
+        setTIMER(TIMESLICE);
+        LDST(&current_process->p_s);
     }else{
         if(process_count==1 && current_process->p_pid==ssi_pcb->p_pid){ 
             HALT();
         }
         if(process_count>1 && soft_block_count>0){
-           setSTATUS(getSTATUS() | STATUS_IEc | STATUS_IM_MASK) & (~STATUS_TE);  
+           setSTATUS((getSTATUS() | STATUS_IEc | STATUS_IM_MASK) & (~STATUS_TE));  
             WAIT(); 
         }
         //deadlock
