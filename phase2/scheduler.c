54531c1d74211f6871f741673d416ccdc4ec5e76
Marta <martabarbieri13@gmail.com>
2024-06-08 18:12:28 +0200

    ...

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -14,25 +14,29 @@ scheduler.c This module implements the Scheduler and the deadlock detector.
 
 
 void scheduler() {
+    //verifica se la coda dei processi è vuota
   	if (emptyProcQ(&ready_queue) == TRUE) {
 		/*
 		current process is always NULL, if the ssi is the only one and has 
 		finished his work is blocked waiting for receiving a message
 		*/
+        //caso in cui l’unico processo è SSI
     	if (process_count == 1 && is_pid_in_list(ssi_pcb->p_pid, &blocked_pcbs[BLOKEDRECV])) {
-      		HALT();
+      		HALT(); // Normal Termination
     	} else if (process_count > 1 && soft_block_count > 0) {
+            //attraverso lo stato del registro abilito gli interrupts e disabilito il PLT
       		setSTATUS((getSTATUS() | STATUS_IEc | STATUS_IM_MASK) & (~STATUS_TE));
-      		WAIT();
+      		WAIT();//il processore non esegue istruzioni in attesa che si verifichi un interrupt 
     	}
     	// deadlock
     	else if (process_count > 0 && soft_block_count == 0) {
-      		PANIC();
+      		PANIC(); //Panic Termination
     	}
   	} else {
+        //rimuovo il PCB dalla coda dei processi pronti 
     	current_process = removeProcQ(&ready_queue);
     	setTIMER(TIMESLICE);
     	STCK(acc_cpu_time); // restart counting
-    	LDST(&current_process->p_s);
+    	LDST(&current_process->p_s);//carica lo stato del processore corrente
   	}
 }
