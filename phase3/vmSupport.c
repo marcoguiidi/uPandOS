822a25c7d3365f64ce5eba02cba7546e9e4fbf3e
Fabio Murer <murer.fabio23@gmail.com>
2024-07-22 18:20:14 +0200

    end pager

--- phase3/vmSupport.c
+++ phase3/vmSupport.c
@@ -1,8 +1,8 @@
 /**
 vmSupport.c: This module implements:
-- the TLB exception handler (The Pager). TODO:
+- the TLB exception handler (The Pager).
 Since reading and writing to each U-proc’s flash device is limited to supporting paging, 
-- this module should also contain the function(s) for reading and writing flash devices. TODO: 
+- this module should also contain the function(s) for reading and writing flash devices. TODO: do ssi part
 Additionally, the Swap Pool table is local to this module. Instead of declaring them globally in initProc.c they can be
 declared module-wide in vmSupport.c. The test function will now invoke a new “public” function
 - initSwapStructs which will do the work of initializing the Swap Pool table. [x]
@@ -24,7 +24,7 @@ exceptions.c file.
 
 swap_t swap_pool[POOLSIZE];
 
-void initSwapStruct(void) {
+void initSwapStructs(void) {
     for (int i = 0; i < POOLSIZE; i++) {
         swap_pool[i].sw_asid = NOPROC;
     }
@@ -44,7 +44,7 @@ int isSwapPoolFrameOccupied(unsigned int framenum) {
 }
 
 unsigned int flashDevWrite(memaddr* write_address, memaddr* flash_dev_address) {
-    unsigned int command = (unsigned int)(write_address << 7) | FLASHWRITE;
+    unsigned int command = (unsigned int)((unsigned int)write_address << 7) | FLASHWRITE;
     unsigned status = 0;
     ssi_do_io_t do_io = {
         .commandAddr = flash_dev_address,
@@ -72,7 +72,7 @@ unsigned int writeFrameToFlashDev(unsigned int frame_number, memaddr *dest_addre
 
 unsigned int flashDevRead(memaddr *read_address, memaddr *flash_dev_address) {
     unsigned int status = 0;
-    unsigned int value = (unsigned int)(read_address << 7) | FLASHREAD;
+    unsigned int value = (unsigned int)((unsigned int)read_address << 7) | FLASHREAD;
 
   ssi_do_io_t do_io = {
       .commandAddr = flash_dev_address,
@@ -95,6 +95,10 @@ unsigned readFrameToFlashDev(memaddr *missing_page_address, pteEntry_t *page) {
     devreg_t* devReg =  (devreg_t*)dev_addr;
     devReg->dtp.data0 = ((page->pte_entryHI & GETPAGENO) >> VPNSHIFT) * PAGESIZE; // ?? bho
     return flashDevWrite(missing_page_address, dev_addr);
+}
+
+void updateTLB(void) {
+    TLBCLR(); // TODO: 5.2 update tlb when all debugged
 }
 
 void pager(void) {
@@ -133,7 +137,7 @@ void pager(void) {
         swap_pool[frame_victim_num].sw_pte->pte_entryLO &= !VALIDON;
 
         // (b)
-        TLBCLR(); // TODO: 5.2 update tlb when all debugged
+        updateTLB();
 
         // (d)
         unsigned int write_status = writeFrameToFlashDev(frame_victim_num, frame_victim_address);
@@ -150,6 +154,21 @@ void pager(void) {
         TrapExceptionHandler(read_status); // ??
     }
 
+    // 10 update swap pool page table
+    swap_pool[frame_victim_num].sw_asid     = support_data->sup_asid;
+    swap_pool[frame_victim_num].sw_pageNo   = missing_page_num;
+    swap_pool[frame_victim_num].sw_pte      = &support_data->sup_privatePgTbl[missing_page_num];
+
+    // 11 update the current process's page table entry
+    support_data->sup_privatePgTbl[missing_page_num].pte_entryLO = ((unsigned int)frame_victim_address << VPNSHIFT) | DIRTYON | VALIDON;
+
+    // 12 upadte the tlb
+    updateTLB();
+
+    // 13
     setSTATUS(saved_status);
     // ATOMIC end
+
+    // 14 return control to che current process
+    LDST(exceptstate);
 }
