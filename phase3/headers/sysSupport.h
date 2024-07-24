8a65e29e945ea8c42a0327b566d9beda16f27013
Fabio Murer <murer.fabio23@gmail.com>
2024-07-24 22:13:03 +0200

    as

---
+++ phase3/headers/sysSupport.h
@@ -0,0 +1,5 @@
+void support_general_exception_handler();
+
+void syscall_exception_handler(unsigned int ExcCode);
+
+void trap_exception_handler(unsigned int ExcCode);
--- phase3/sysSupport.c
+++ phase3/sysSupport.c
@@ -4,3 +4,33 @@ sysSupport.c: This module implements the Support Level’s:
 - SYSCALL exception handler [Section 8].
 - Program Trap exception handler [Section 9].
 */
+
+#include "./headers/sysSupport.h"
+#include "./headers/misc.h"
+#include <umps3/umps/types.h>
+#include <umps3/umps/const.h>
+#include <umps3/umps/arch.h>
+#include <umps3/umps/libumps.h>
+#include <umps3/umps/cp0.h>
+
+void support_general_exception_handler() {
+    state_t* exception_state = (state_t*)BIOSDATAPAGE; // ??
+    unsigned int cause  = exception_state->cause;
+    unsigned int ExcCode = CAUSE_GET_EXCCODE(cause);
+
+    if (ExcCode >= 1) { // ?
+        syscall_exception_handler(ExcCode);
+    } else {
+        trap_exception_handler(ExcCode);
+    }
+}
+
+void syscall_exception_handler(unsigned int ExcCode) {
+
+}
+
+void trap_exception_handler(unsigned int ExcCode) {
+
+}
+
+void 
